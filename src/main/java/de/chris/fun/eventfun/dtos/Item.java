286ae91f72890553b92c8e2ccbe9f516884127c0
Christian Wander <chris.wander@posteo.de>
2017-09-14 21:46:25 +0200

    format cleanup and tostring

--- src/main/java/de/chris/fun/eventfun/dtos/Item.java
+++ src/main/java/de/chris/fun/eventfun/dtos/Item.java
@@ -12,39 +12,44 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Item {
 
-    private String sku;
-    private String price;
-    private String currency;
+	private String sku;
+	private String price;
+	private String currency;
 
-    public Item(String sku, String price, String currency) {
-        super();
-        this.sku = sku;
-        this.price = price;
-        this.currency = currency;
-    }
+	public Item(String sku, String price, String currency) {
+		super();
+		this.sku = sku;
+		this.price = price;
+		this.currency = currency;
+	}
 
-    public String getSku() {
-        return sku;
-    }
+	public String getSku() {
+		return sku;
+	}
 
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
 
-    public String getPrice() {
-        return price;
-    }
+	public String getPrice() {
+		return price;
+	}
 
-    public void setPrice(String price) {
-        this.price = price;
-    }
+	public void setPrice(String price) {
+		this.price = price;
+	}
 
-    public String getCurrency() {
-        return currency;
-    }
+	public String getCurrency() {
+		return currency;
+	}
 
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [sku=" + sku + ", price=" + price + ", currency=" + currency + "]";
+	}
 
 }
