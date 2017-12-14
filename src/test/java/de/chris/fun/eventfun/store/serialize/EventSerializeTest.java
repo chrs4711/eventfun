676e68483cd726d3e5a72508b521af9dfcf11586
Christian Wander <christian.wander@hr-group.de>
2017-12-14 18:00:08 +0100

    improved EventSerializer test

--- src/test/java/de/chris/fun/eventfun/store/serialize/EventSerializeTest.java
+++ src/test/java/de/chris/fun/eventfun/store/serialize/EventSerializeTest.java
@@ -12,13 +12,13 @@
  */
 package de.chris.fun.eventfun.store.serialize;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import de.chris.fun.eventfun.domainevents.CartCreatedEvent;
-import de.chris.fun.eventfun.store.serialize.DomainEventSerializ0r;
-import de.chris.fun.eventfun.store.serialize.JsonSerializ0r;
 
 /**
  * @author Christian Wander
@@ -29,11 +29,14 @@ public class EventSerializeTest {
 
     private final DomainEventSerializ0r s = new JsonSerializ0r();
 
+    private static final String CART_CREATED_JSON = "{\"createdBy\":\"nobody\"}";
+
     @Test
     public void test() {
 
         final CartCreatedEvent e = new CartCreatedEvent("nobody");
         final String actual = s.serialize(e);
+        assertEquals(CART_CREATED_JSON, actual);
 
         System.out.println();
         System.out.println(actual);
