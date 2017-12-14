0c8019d2ede222a74149a0ab7e796e8527550271
Christian Wander <christian.wander@hr-group.de>
2017-12-14 11:58:23 +0100

    minor cleanup

--- src/main/java/de/chris/fun/eventfun/controller/CartController.java
+++ src/main/java/de/chris/fun/eventfun/controller/CartController.java
@@ -3,11 +3,11 @@ package de.chris.fun.eventfun.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import de.chris.fun.eventfun.dtos.Item;
 import de.chris.fun.eventfun.service.CartService;
@@ -19,11 +19,9 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
-    // private static final Logger logger = LoggerFactory.getLogger(CartController.class);
-
     @PostMapping("")
     public ResponseEntity<?> createNewCart() {
-        
+
         return ResponseEntity.ok(cartService.createCart());
     }
 
@@ -33,18 +31,15 @@ public class CartController {
         return ResponseEntity.ok(cartService.getCart(cartId));
     }
 
-    @RequestMapping(value = "{cartId}/items", method = RequestMethod.POST)
+    @PostMapping("{cartId}/items")
     public ResponseEntity<?> addItemToCart(String cartId, Item item) {
-        cartService.addItemToCart(cartId, item);
 
         return ResponseEntity.ok(cartService.addItemToCart(cartId, item));
     }
 
-    @RequestMapping(value = "{cartId}/items/{sku}", method = RequestMethod.DELETE)
+    @DeleteMapping("{cartId}/items/{sku}")
     public ResponseEntity<?> deleteItemFromCart(@PathVariable("cartId") String cartId,
             @PathVariable("sku") String sku) {
-
-        
 
         return ResponseEntity.ok(cartService.deleteItemFromCart(sku, cartId));
     }
