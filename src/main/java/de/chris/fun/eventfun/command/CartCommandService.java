81e22a2b7ea729712f9c38dda2d8b6716913aaaa
Christian Wander <chris.wander@posteo.de>
2017-12-18 16:17:03 +0100

    moved CommandService to own package and created further domain events

--- src/main/java/de/chris/fun/eventfun/command/CartCommandService.java
+++ src/main/java/de/chris/fun/eventfun/command/CartCommandService.java
@@ -1,9 +1,10 @@
-package de.chris.fun.eventfun.domainevents;
+package de.chris.fun.eventfun.command;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import de.chris.fun.eventfun.domainevents.CartCreated;
 import de.chris.fun.eventfun.dtos.Item;
 import de.chris.fun.eventfun.store.EventStore;
 
